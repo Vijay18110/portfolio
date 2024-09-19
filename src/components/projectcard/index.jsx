42ea0531a54986778ded7ec314fbf57af7e4ac90
Vijay18110 <vijay9628554907@gmail.com>
2024-09-20 03:10:28 +0530

    changes

--- src/components/projectcard/index.jsx
+++ src/components/projectcard/index.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import styles from './index.module.css'
 import Button from '../../widgets/button'
 
-const ProjCarc = ({ name, tech, img }) => {
+const ProjCarc = ({ name, tech, img, link }) => {
     return (
         <>
             <div className={styles.projcardcont}>
                 <img src={img} alt="project" />
-                <div className={styles.btncard}><Button value="visit"></Button></div>
-
+                <a href={link}><div className={styles.btncard}><Button value="visit"></Button></div></a>
                 <div className={styles.aboutprojectonhover}>
                     <div>{name}</div>
                     <div>{tech}</div>
--- src/pages/Project/index.jsx
+++ src/pages/Project/index.jsx
@@ -3,6 +3,7 @@ import Nav from '../../components/Navrbar'
 import styles from './index.module.css'
 import Footer from '../../components/Footer'
 import ProjCarc from '../../components/projectcard'
+import { Link } from 'react-router-dom'
 const Project = () => {
     return (
         <>
@@ -11,13 +12,15 @@ const Project = () => {
                 <h1>Projects</h1>
             </div>
             <div className={styles.Projectcont}>
-                <ProjCarc name="Student greivance system with" img="Screenshot1.png" tech="react.js & node.js"></ProjCarc>
+                <ProjCarc link="https://student-grievance.netlify.app/" name="Student greivance system with" img="Screenshot1.png" tech="react.js & node.js"></ProjCarc>
+                <ProjCarc link="https://vijayatm.netlify.app/" name="ATM app functionality with localstorage" img="Screenshotatm.png" tech="react.js & node.js"></ProjCarc>
+                {/* <a target='_target' href="https://vijayatm.netlify.app/"></a> */}
                 <ProjCarc name="Note app clone" img="noteapp clone.png" tech="react.js"></ProjCarc>
                 <ProjCarc name="text-editor app with all functionality" img="Screenshot3.png" tech="react.js"></ProjCarc>
-                <ProjCarc name="unbrew " img="Screenshot4.png" tech="react.js"></ProjCarc>
-                <ProjCarc name="My portfolio website" img="Screenshot5.png" tech="react.js"></ProjCarc>
+                <ProjCarc link='https://main--unbrew.netlify.app/' name="unbrew " img="Screenshot4.png" tech="react.js"></ProjCarc>
+                <ProjCarc link="https://main--vnportfolio.netlify.app/home" name="My portfolio website" img="Screenshot5.png" tech="react.js"></ProjCarc>
                 <ProjCarc name="flipkart with all functionality with backend" img="Screenshot6.png" tech="react.js & node.js"></ProjCarc>
-                <ProjCarc name="password generator of all charactor" img="Screenshot7.png" tech="react.js"></ProjCarc>
+                <ProjCarc link="https://main--vnpasswordgenerator.netlify.app/" name="password generator of all charactor" img="Screenshot7.png" tech="react.js"></ProjCarc>
                 <ProjCarc name="social-media app like instagram to create simple post" img="Screenshot8.png" tech="react.js"></ProjCarc>
                 <ProjCarc name="todo app to maintain your routine" img="Screenshot9.png" tech="react.js"></ProjCarc>
                 <ProjCarc name="Student greivance system with" img="Screenshot1.png" tech="react.js & node.js"></ProjCarc>
@@ -29,7 +32,7 @@ const Project = () => {
                 <ProjCarc name="Student greivance system with" img="Screenshot1.png" tech="react.js & node.js"></ProjCarc>
                 <ProjCarc name="Student greivance system with" img="Screenshot1.png" tech="react.js & node.js"></ProjCarc>
                 <ProjCarc name="Student greivance system with" img="Screenshot1.png" tech="react.js & node.js"></ProjCarc>
-            </div>
+            </div >
             <div className={styles.Footercont}>
                 <Footer></Footer>
             </div>
